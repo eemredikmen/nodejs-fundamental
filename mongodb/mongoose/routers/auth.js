4415a8941523b1cc59c938c2c55a29b144a8db57
Yunus Emre Dikmen <emredikmen002@gmail.com>
2020-12-30 23:42:30 +0300

    delete

--- mongodb/mongoose/routers/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const {register} = require('../controllers/auth.js');
-const router = express.Router();
-
-router.get('/',register);
-router.post('/register',register);
-
-
-module.exports = router;
--- mongodb/mongoose/routers/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const questions = require('./questions');
-const auth = require('./auth');
-
-router.use("/auth", auth);
-router.use("/questions", questions);
-
-module.exports = router;
